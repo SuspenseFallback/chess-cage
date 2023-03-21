d80c9d67de0eccb367cd25aad66f2048ecf32452
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-03-21 19:07:41 +0800

    stuff

--- src/views/Lessons.jsx
+++ src/views/Lessons.jsx
@@ -7,21 +7,12 @@ const Lessons = () => {
   return (
     <>
       <div className="lessons-container">
-        <div className="beginner">
-          <p className="header">Beginner</p>
-          <p className="item-title">The pieces</p>
+        <div className="select">
           <ul>
-            <li className="item">
-              <img src={wpawn} alt="Pawn" className="img" width="50" />
-              <p className="item-header">The Pawn</p>
-            </li>
+            <li>Beginner</li>
+            <li>Intermediate</li>
+            <li>Advanced</li>
           </ul>
-        </div>
-        <div className="intermediate">
-          <p className="header">Intermediate</p>
-        </div>
-        <div className="advanced">
-          <p className="header">Advanced</p>
         </div>
       </div>
     </>
